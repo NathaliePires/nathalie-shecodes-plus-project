3097df08e6f943125e839f0e28d4abcf707f4ef5
Nathalie <nathalienspires@gmail.com>
2021-08-22 04:52:16 -0300

    My project until the fifth week

---
+++ script.js
@@ -0,0 +1,59 @@
+let currentTime = new Date();
+let small = document.querySelector("small");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let currentDay = days[currentTime.getDay()];
+let currentHours = currentTime.getHours();
+let currentMinutes = currentTime.getMinutes();
+
+small.innerHTML = `${currentDay} ${currentHours}:${currentMinutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-text-input");
+
+  let apiKey = "ceabf21fba1b01fe952de21a0e2fb97f";
+  let city = `${searchInput.value}`;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let city = response.data.name;
+
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${temperature}`;
+
+  let h5 = document.querySelector("h5");
+  h5.innerHTML = `${city}`;
+}
+
+function currentPosition(position) {
+  let longitude = position.coords.longitude;
+  let latitude = position.coords.latitude;
+
+  let apiKey = "ceabf21fba1b01fe952de21a0e2fb97f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+}
+
+function getCurrLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(currentPosition);
+}
+
+let currButton = document.querySelector("#currentPositionBtn");
+currButton.addEventListener("click", getCurrLocation);
