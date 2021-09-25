ca7bfca46505fa438209eb30051f14e88ebe8e64
Nathalie <nathalienspires@gmail.com>
2021-09-25 05:43:07 -0300

    I did the layout of the week's forecasts, add APIs, fixed some bugs and the "ul" layout.

--- script.js
+++ script.js
@@ -1,25 +1,74 @@
-let currentTime = new Date();
-let small = document.querySelector("small");
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-let currentDay = days[currentTime.getDay()];
-let currentHours = currentTime.getHours();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 
-let currentMinutes = currentTime.getMinutes();
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
-small.innerHTML = `${currentDay} ${currentHours}:${currentMinutes}`;
-let form = document.querySelector("#search-form");
-let celsiusTemperature = null;
-let fahrenheitLink = document.querySelector("#fahr-link");
-let celsiusLink = document.querySelector("#celsius-link");
-let currButton = document.querySelector("#currentPositionBtn");
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `<div class="col-2">
+        <div class="row-date">${formatDay(forecastDay.dt)}</div>
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecastDay.weather[0].icon
+          }@2x.png"
+          alt=""
+          width="42"
+        />
+        <div class="row-temps">
+          <span class="row-temp-max"> ${Math.round(
+            forecastDay.temp.max
+          )}° </span>
+          <span class="row-temp-min"> ${Math.round(
+            forecastDay.temp.min
+          )}° </span>
+        </div>
+      </div>`;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "ceabf21fba1b01fe952de21a0e2fb97f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
 
 function search(event) {
   event.preventDefault();
@@ -40,6 +89,7 @@ function showTemperature(response) {
   let description = document.querySelector("#description");
   let humidity = document.querySelector("#humidity");
   let wind = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
 
   h1.innerHTML = `${temperature}`;
@@ -47,12 +97,15 @@ function showTemperature(response) {
   description.innerHTML = response.data.weather[0].description;
   humidity.innerHTML = response.data.main.humidity;
   wind.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 
   iconElement.setAttribute(
     "src",
     `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function displayFahrenheitTemperature(event) {
@@ -85,7 +138,14 @@ function getCurrLocation(event) {
   navigator.geolocation.getCurrentPosition(currentPosition);
 }
 
+let celsiusTemperature = null;
+let form = document.querySelector("#search-form");
 form.addEventListener("submit", search);
+let fahrenheitLink = document.querySelector("#fahr-link");
 fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
+let currButton = document.querySelector("#currentPositionBtn");
 currButton.addEventListener("click", getCurrLocation);
+
+search("Porto Alegre");
